e6f1667699d70a5742747a463be8722af3422d0b
russellw999 <29832103+russellw999@users.noreply.github.com>
2022-10-25 15:43:08 +0200

    ch02 Creating First component and props

--- feedback-app-main/src/App.jsx
+++ feedback-app-main/src/App.jsx
@@ -1,11 +1,25 @@
 import React, { Fragment } from 'react';
+import Header from './components/Header';
+import PropTypes from 'prop-types';
 
 const App = () => {
   return (
     <Fragment>
-      <div>Hello from Russells App</div>
+      <Header text={true} />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
     </Fragment>
   );
+};
+
+// Default for Props
+Header.defaultProps = {
+  text: 'Feedback UI',
+};
+
+Header.prototype = {
+  text: PropTypes.string,
 };
 
 export default App;
---
+++ feedback-app-main/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = ({ text }) => {
+  return (
+    <header>
+      <div>
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
