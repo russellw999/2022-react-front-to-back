a24ecd0554bd3f246698eeba4ec1bf0d9c93c4ac
russellw999 <29832103+russellw999@users.noreply.github.com>
2022-10-27 14:05:21 +0200

    ch05 Creating Routes v5

--- feedback-app-main/src/App.jsx
+++ feedback-app-main/src/App.jsx
@@ -1,3 +1,5 @@
+import AboutPage from './components/pages/AboutPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
@@ -28,12 +30,17 @@ const App = () => {
 
   return (
     <Fragment>
-      <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Route exact path="/">
+            <FeedbackForm handleAdd={addFeedback} />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+          </Route>
+          <Route path="/about" component={AboutPage} />
+        </div>
+      </Router>
     </Fragment>
   );
 };
