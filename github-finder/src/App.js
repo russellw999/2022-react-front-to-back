c435918e68f8b1244d554e789ed07286f149d6c4
russellw999 <29832103+russellw999@users.noreply.github.com>
2022-10-30 22:53:01 +0100

    ch08 Footer Component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
 
-          <main>Content</main>
+          <main className="container mx-auto px-3 pb-12">Content</main>
+          <Footer />
         </div>
       </Fragment>
     </Router>
