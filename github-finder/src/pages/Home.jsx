7f9796c1928c230cb5755789e36ff40156c7b154
russellw999 <29832103+russellw999@users.noreply.github.com>
2022-10-31 10:01:51 +0100

    ch08 Pages and Routes completed

---
+++ github-finder/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1 className="text-6xl">Welcome</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
