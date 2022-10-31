fb962325cf587ed7c4de84bde2a850edbcd5f3f0
russellw999 <29832103+russellw999@users.noreply.github.com>
2022-10-31 19:09:12 +0100

    ch09 UserList component

---
+++ github-finder/src/components/users/UserResults.jsx
@@ -0,0 +1,39 @@
+import { Fragment, useEffect, useState } from 'react';
+
+const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+    //console.log(data);
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <Fragment>
+        <div className="grid grid-cols-1 gap-8 xl:grod-cols-4 lg:grid-cols-3 md:grid-cols-2">
+          {users.map((user) => (
+            <h3 key={user.id}>{user.login}</h3>
+          ))}
+        </div>
+      </Fragment>
+    );
+  } else {
+    <h3>Loading.....</h3>;
+  }
+};
+
+export default UserResults;
--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
+import UserResults from '../components/users/UserResults';
 
 const Home = () => {
   return (
     <Fragment>
       <div>
-        <h1 className="text-6xl">Welcome</h1>
+        {/* SEARCH COMPONENT */}
+        <UserResults />
         {process.env.REACT_APP_GITHUB_TOKEN}
       </div>
     </Fragment>
