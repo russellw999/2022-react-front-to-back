bcea69ca94dece390a6d731cd29390c4468cde75
russellw999 <29832103+russellw999@users.noreply.github.com>
2022-11-20 16:44:45 +0100

    ch12 useRef Example1

---
+++ react-sandbox/src/App.js
@@ -0,0 +1,10 @@
+import UseRefExampl1 from './components/UseRefExampl1';
+const App = () => {
+  return (
+    <div className="container mt-5">
+      <UseRefExampl1 />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-sandbox/src/components/UseRefExampl1.jsx
@@ -0,0 +1,31 @@
+import { useRef } from 'react';
+
+const UseRefExampl1 = () => {
+  const inputRef = useRef();
+  const paraRef = useRef();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    inputRef.current.value += ' Russell';
+    inputRef.current.style.backgroundColor = 'red';
+    paraRef.current.innerText = 'Goodbye';
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="name">Name</label>
+        <input type="text" ref={inputRef} className="form-control mb-2" />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <p ref={paraRef} onClick={() => inputRef.current.focus()}>
+          Hello
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default UseRefExampl1;
